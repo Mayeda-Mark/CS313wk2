405745a642945650b6c9bd4e1e112db5d7a8f999
Mayeda-Mark <markrmayeda@gmail.com>
2018-09-29 23:22:54 -0700

    ok, one last try

---
+++ web/homepage.js
@@ -0,0 +1,57 @@
+// homepage.js
+
+function family() {
+	document.getElementById("family").innerHTML = "I live With my Wife, Abi and our 2 kids, George(2) and Cora(4).";
+}
+
+function familyOut() {
+	document.getElementById("family").innerHTML = "Family";
+}
+
+function home() {
+	document.getElementById("home").innerHTML = "I live in Auburn, WA.  Just south of Seattle.";
+}
+
+function homeOut() {
+	document.getElementById("home").innerHTML = "Home";
+}
+
+function work(){
+	document.getElementById("work").innerHTML = "I work at the Federal Aviation Administration as a flight data specialist.";
+}
+
+function workOut(){
+	document.getElementById("work").innerHTML = "Work";
+}
+
+function hobbies(){
+	document.getElementById("hobbies").innerHTML = "In my free time, I like to read, watch TV and movies, play video games and guitar and surf, snowboard, hike and camp.";
+}
+
+function hobbiesOut(){
+	document.getElementById("hobbies").innerHTML = "Hobbies";
+}
+
+function books(){
+	document.getElementById("favBooks").innerHTML = "I love the Dresden Files books, the Game of Thrones Books and pretty much everything by Rick Riordan.";
+}
+
+function booksOut(){
+	document.getElementById("favBooks").innerHTML = "Favorite Books";
+}
+
+function TV(){
+	document.getElementById("favTVShows").innerHTML = "I love Agents of S.H.I.E.L.D., the Good Place, Star Trek, the Simpsons and Last Week Tonight.";
+}
+
+function TVOut(){
+	document.getElementById("favTVShows").innerHTML = "Favorite TV Shows";
+}
+
+function movies(){
+	document.getElementById("favMovies").innerHTML = "I'm a huge fan of all of the MCU movies";
+}
+
+function moviesOut(){
+	document.getElementById("favMovies").innerHTML = "Favorite Movies";
+}
